1a9040cee771bc249e1b786a5e35b7abd5135ae0
bevchou <31548243+bevchou@users.noreply.github.com>
2018-04-30 21:52:31 -0400

    works wtih enter key too

--- w7-final/popup.js
+++ w7-final/popup.js
@@ -1,12 +1,34 @@
 let msg;
+let timest;
 
+//execute when the user clicks submit
 document.getElementById("submit").addEventListener("click", function() {
   console.log("submit button clicked!");
+  getInfo();
 
-  msg = document.getElementById('userText').value;
-  console.log("new message: ");
-  console.log(msg);
 });
-// document.getElementById("submit").addEventListener("click", function() {
-//     console.log("hello world");
-// });
+
+//execute when the user hits enter
+document.getElementById("userText").onkeypress = function(e) {
+  if (!e) e = window.event;
+  var keyCode = e.keyCode || e.which;
+  if (keyCode == "13"){
+    console.log("enter key pressed!");
+    getInfo();
+    return false;
+  }
+}
+
+
+
+function getInfo() {
+  //get the current time
+  timest = new Date();
+  console.log("time:" + timest);
+  //get the user's message
+  msg = document.getElementById("userText").value;
+  console.log("new message: " + msg);
+
+  document.getElementById("userText").value = "";
+
+}
